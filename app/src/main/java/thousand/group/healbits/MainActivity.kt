71b6d262b04ea53dd8075587bd29dece8ad98088
Kanat Turganbay <kanatturganbay@gmail.com>
2020-04-14 00:39:27 +0500

    First Commit

---
+++ app/src/main/java/thousand/group/healbits/MainActivity.kt
@@ -0,0 +1,12 @@
+package thousand.group.healbits
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
